68e38906a3b8523ad874ad9199ed8e2a750b633f
indigoxela <user@olga.indigofloat.at>
2021-07-28 16:00:23 +0200

    Switch to behaviors and prevent from choking in maintenance mode

--- js/maildisguise.js
+++ js/maildisguise.js
@@ -1,14 +1,22 @@
 (function ($) {
-  $(document).ready(function() {
-    let myclassname = Backdrop.settings.maildisguise.class;
-    //console.log(myclassname);
-    $('.' + myclassname).each(function () {
-      let decoded = $(this).attr('data-href').replace(/[a-zA-Z]/g, function(char) {
-        return String.fromCharCode((char <= "Z" ? 90 : 122) >= (char = char.charCodeAt(0) + 13) ? char : char - 26);
+  Backdrop.behaviors.mailDisguise = {
+
+    attach: function (context, settings) {
+      if (typeof settings.maildisguise === 'undefined') {
+        return;
+      }
+
+      let myclassname = settings.maildisguise.class;
+
+      $('.' + myclassname).each(function () {
+        let decoded = $(this).attr('data-href').replace(/[a-zA-Z]/g, function(char) {
+          return String.fromCharCode((char <= "Z" ? 90 : 122) >= (char = char.charCodeAt(0) + 13) ? char : char - 26);
+        });
+        let replaced = decoded.replace(/^#/, 'mailto:').replace(/\/at\//, '@');
+        $(this).attr('href', replaced);
       });
-      let replaced = decoded.replace(/^#/, 'mailto:').replace(/\/at\//, '@');
-      $(this).attr('href', replaced);
-    });
-    $('.' + myclassname + '-inner > span').replaceWith('@');
-  });
+      $('.' + myclassname + '-inner > span').replaceWith('@');
+    }
+
+  }
 })(jQuery);
